bc9573441ffbf6524a3bf4d64996a696d2050857
yehor <kravtsov.egor@kronas.com.ua>
2024-11-02 21:33:17 +0100

    Added prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,32 @@
-import {defineConfig} from 'vite'
-import {URL, fileURLToPath, resolve} from "node:url";
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { URL, fileURLToPath, resolve } from "node:url";
+import react from "@vitejs/plugin-react";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-        open: true,
-
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+    open: true,
+  },
+  build: {
+    target: "modules", // https://vitejs.dev/config/build-options#build-target
+    sourcemap: "hidden", // https://vitejs.dev/config/build-options#build-sourcemap
+    chunkSizeWarningLimit: 600, // https://vitejs.dev/config/build-options#build-chunksizewarninglimit
+    minify: "esbuild", // https://vitejs.dev/config/build-options#build-minify
+    cssMinify: "lightningcss", // https://vitejs.dev/config/build-options#build-cssminify,
+    cssCodeSplit: true, // https://vitejs.dev/config/build-options#build-csscodesplit
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
     },
-    preview: {
-        port: 8080,
-        strictPort: true,
-        open: true
-    },
-    build: {
-        target: "modules", // https://vitejs.dev/config/build-options#build-target
-        sourcemap: "hidden", // https://vitejs.dev/config/build-options#build-sourcemap
-        chunkSizeWarningLimit: 600, // https://vitejs.dev/config/build-options#build-chunksizewarninglimit
-        minify: "esbuild", // https://vitejs.dev/config/build-options#build-minify
-        cssMinify: "lightningcss", // https://vitejs.dev/config/build-options#build-cssminify,
-        cssCodeSplit: true // https://vitejs.dev/config/build-options#build-csscodesplit
-    },
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "./src"),
-        }
-    }
-})
+  },
+});
